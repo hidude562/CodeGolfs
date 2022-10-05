84b9458dec0fe2706b63b5f2dab8f428d4596137
Nathan M <82677882+hidude562@users.noreply.github.com>
2022-10-05 07:09:23 -0400

    💀💀 i fogor to rename the method call in the main method 💀

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -3,6 +3,6 @@ class CollatzConjecture {
    static void r(int n){System.out.println(n);n=(1+3*n)*(n%2)+(n+1)%2*n/2;if(n!=1)r(n);}
    
    public static void main(String[] args) {
-      run(49);
+      r(49);
    }
 }
